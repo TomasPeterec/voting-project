849ff9b023c31282a17e4ee45ed0f7913c6b0f83
TomasPeterec <info@tomaspeterec.sk>
2023-03-06 02:32:51 +0100

    main structure refactored

--- src/App.js
+++ src/App.js
@@ -4,139 +4,147 @@ import ItemForVoting from './ItemForVoting';
 import { useState } from 'react';
 
 
-const listOfPosibilities = [
-  {title: "morské prasa", description: "Someting 1", votingValue: 0},
-  {title: "škrečok", description: "Someting 2", votingValue: 0},
-  {title: "užovka červená", description: "Someting 3", votingValue: 0},
-  {title: "papagáj", description: "Someting 4", votingValue: 0},
-  {title: "skunk", description: "Someting 5", votingValue: 0},
-  {title: "mravce", description: "Someting 6", votingValue: 0},
-  {title: "vakoveverica", description: "Someting 7", votingValue: 0},
-  {title: "pes", description: "Someting 8", votingValue: 0},
-  {title: "rybička", description: "Someting 8", votingValue: 0}
-]
 
-const listOfCheckboxes = listOfPosibilities.map(() => false)
+
 const maxPercentForOne = 50
 const minPercentForOne = 1 //it must not be absolute zero
 const percent100 = 100
 const nextFloat = 1
-const initialValue = percent100 / listOfPosibilities.length
 
-
-function App() {
-  const [firstRun, setFirstRun] = useState(true)
-  if (firstRun === true) {
-    listOfPosibilities.forEach(item01 => {
-      item01.votingValue = initialValue
-    })
-    setFirstRun(false)
-  }
-
-  const [copyOfPosibilities, setCopyOfPosibilities] = useState(listOfPosibilities)
-  const [checkboxList, setCheckboxList] = useState(listOfCheckboxes)
-
-  //main counting function
-  const percentageDistribution = (ar, curentSlider) => {
+//main counting function
+const percentageDistribution = (ar, curentSlider) => {
    
-    var ar1 = [...ar] //copiing of array of posibilities
-    var countOfAll = 0
-    var valueToRatioTo100 = 0 //a ratio with which are uncecked and unselected multiplied 
-    var valueOfChecked = 0
-    //test22 for testing
-    // var count22 = 0
-  
-    checkboxList.forEach((item, index) => {
-      if(item === true){
-        if(index !== curentSlider){
-          // console.log('index under condition: '+index)
-        valueOfChecked = valueOfChecked + Number(ar1[index].votingValue)
-        }
-      }
-      // console.log('value of checked: '+valueOfChecked)
-      // console.log('index: '+index)
-    })
+  let ar1 = [...ar] //copiing of array of posibilities
+  let countOfAll = 0
+  let valueToRatioTo100 = 0 //a ratio with which are uncecked and unselected multiplied 
+  let valueOfChecked = 0
+  //test22 for testing
+  // let count22 = 0
 
-  
-    for(let i = 0; i < ar1.length; i++) {
-      countOfAll = Number(ar1[i].votingValue) + countOfAll
-    }
-
-    var valueToSubteact = Number(ar1[curentSlider].votingValue) + Number(valueOfChecked)
-    valueToRatioTo100 = (percent100-valueToSubteact)/(countOfAll-valueToSubteact)
-
-    for(let i = 0; i < ar1.length ; i++) {
-      if(checkboxList[i] === false){
-        if(i !== curentSlider){
-          ar1[i].votingValue = ar1[i].votingValue * valueToRatioTo100
-        } 
+  ar1.forEach((item, index) => {
+    //console.log(item.fixed)
+    if(item.fixed === true){
+      if(index !== curentSlider){
+        // console.log('index under condition: '+index)
+        valueOfChecked += Number(ar1[index].votingValue)
       }
     }
+    // console.log('value of checked: '+valueOfChecked)
+    // console.log('index: '+index)
+  })
 
-    //this is condition part just for checking if any slider value is not more than maxPercentForOne
-    for(let s3 = 0; s3 < ar1.length; s3++) {
-      if(ar1[s3].votingValue > maxPercentForOne){
-      //test
-      // console.log(ar1[s3].votingValue)
-      ar1[s3].votingValue = maxPercentForOne
-      //test
-      // console.log(ar1[s3].votingValue)
-        ar1 = percentageDistribution(ar1, s3)
-      }else{
-        if(ar1[s3].votingValue < minPercentForOne){
-          ar1[s3].votingValue = minPercentForOne
-          ar1 = percentageDistribution(ar1, s3)
-        }
-      }
-    }
 
-    // //test22 this was just a test if the number of all slider values ​​is 100
-    // for(let i = 0; i < ar1.length ; i++) {
-    //   count22 = Number(ar1[i].votingValue) + count22
-    // }
-    // console.log('sum of all: '+count22)
-
-    return ar1
+  for(let i = 0; i < ar1.length; i++) {
+    countOfAll += Number(ar1[i].votingValue)
   }
+
+  let valueToSubteact = Number(ar1[curentSlider].votingValue) + Number(valueOfChecked)
+  valueToRatioTo100 = (percent100-valueToSubteact)/(countOfAll-valueToSubteact)
+
+  for(let i = 0; i < ar1.length ; i++) {
+
+    if(ar1[i].fixed !== true){
+      if(i !== curentSlider){
+        ar1[i].votingValue = ar1[i].votingValue * valueToRatioTo100
+      } 
+    }else{
+      //console.log(ar1[i].votingValue)
+    }
+  }
+
+  //this is condition part just for checking if any slider value is not more than maxPercentForOne
+  for(let s3 = 0; s3 < ar1.length; s3++) {
+    if(ar1[s3].votingValue > maxPercentForOne){
+    //test
+    // console.log(ar1[s3].votingValue)
+    ar1[s3].votingValue = maxPercentForOne
+    //test
+    // console.log(ar1[s3].votingValue)
+      ar1 = percentageDistribution(ar1, s3)
+    }else{
+      if(ar1[s3].votingValue < minPercentForOne){
+        ar1[s3].votingValue = minPercentForOne
+        ar1 = percentageDistribution(ar1, s3)
+      }
+    }
+  }
+
+  // //test22 this was just a test if the number of all slider values ​​is 100
+  // for(let i = 0; i < ar1.length ; i++) {
+  //   count22 = Number(ar1[i].votingValue) + count22
+  // }
+  // console.log('sum of all: '+count22)
+
+  return ar1
+}
+
+function App({possibilities}) {
+ // const listOfCheckboxes = possibilities.map(() => false)
+
+  possibilities.forEach((item, index) => {
+    if(item.fixed === undefined){
+      possibilities[index].fixed = false
+    }
+  })
+
+
+  const [copyOfPosibilities, setCopyOfPosibilities] = useState(possibilities)
+ // const [checkboxList, setCheckboxList] = useState(listOfCheckboxes)
+
+  
 
   return (
     <div className="App">
         {
           //main loop which generates the rows
-          copyOfPosibilities.map((item, i) => 
-          <div style={{display: 'inline-flex'}}>
-            <ItemForVoting key={i}
-              title={item.title}
-              description={item.description}
-              votingValue={copyOfPosibilities[i].votingValue}
-            />
-          <div style={{display: 'inline-flex'}}>
-            <div key={i}>
-            {Math.round(copyOfPosibilities[i].votingValue*nextFloat)/nextFloat}%
+          copyOfPosibilities.map((possibility, i) => 
+            <div style={{display: 'inline-flex'}}>
+              <ItemForVoting key={i}
+                title={possibility.title}
+                description={possibility.description}
+               // votingValue={copyOfPosibilities[i].votingValue}
+              />
+              <div style={{display: 'inline-flex'}}>
+                <div key={i}>
+                  {Math.round(possibility.votingValue * nextFloat) / nextFloat}%
+                </div>
+              
+                <input value={possibility.votingValue} 
+                  type="range" 
+                  min="0" 
+                  max={maxPercentForOne}
+                  step="0.0001" 
+                  onChange={(event) => {
+                    const copy1 = [...copyOfPosibilities]
+                    copy1[i].votingValue = event.target.value
+                    setCopyOfPosibilities(percentageDistribution(copy1, i))
+                  }}
+                />
+                {/* <input type='checkbox' onChange={() => {
+                  let copy222 = [...checkboxList]
+                  if (checkboxList[i] === true){
+                    copy222[i] = false
+                  }else{
+                    copy222[i] = true
+                  }
+                  setCheckboxList(copy222)
+                }} /> */}
+
+                <input 
+                  type="checkbox"
+                  defaultChecked={copyOfPosibilities[i].fixed}
+                  onChange={(e) => {
+                    const newPossibilities = [...copyOfPosibilities]
+                    newPossibilities[i].fixed = e.target.checked
+                    //console.log(e.target.checked)
+                    //console.log(newPossibilities[i].fixed)
+                    //debugger
+                    setCopyOfPosibilities(newPossibilities)
+                  }} 
+                />
+
+              </div>
             </div>
-           
-            <input value={copyOfPosibilities[i].votingValue} 
-              type="range" 
-              min="0" 
-              max={maxPercentForOne}
-              step="0.0001" 
-              onChange={(event) => {
-                let copy1 = [...copyOfPosibilities]
-                copy1[i].votingValue = event.target.value
-                copy1 = percentageDistribution(copy1,i)
-                setCopyOfPosibilities(copy1)
-              }}
-            /><input type='checkbox' onChange={() => {
-              let copy222 = [...checkboxList]
-              if (checkboxList[i] === true){
-                copy222[i] = false
-              }else{
-                copy222[i] = true
-              }
-              setCheckboxList(copy222)
-            }} ></input>
-          </div>
-          </div>
         )}
       
     </div>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const VOTINGVALUE = 100 / 9
+// TODO: convert to uppercase
+const DUMMY_POSSIBILIES = [
+  {title: "morské prasa", description: "Someting 1", votingValue: VOTINGVALUE},
+  {title: "škrečok", description: "Someting 2", votingValue: VOTINGVALUE},
+  {title: "užovka červená", description: "Someting 3", votingValue: VOTINGVALUE},
+  {title: "papagáj", description: "Someting 4", votingValue: VOTINGVALUE},
+  {title: "skunk", description: "Someting 5", votingValue: VOTINGVALUE},
+  {title: "mravce", description: "Someting 6", votingValue: VOTINGVALUE},
+  {title: "vakoveverica", description: "Someting 7", votingValue: VOTINGVALUE},
+  {title: "pes", description: "Someting 8", votingValue: VOTINGVALUE},
+  {title: "rybička", description: "Someting 8", votingValue: VOTINGVALUE}
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App possibilities={DUMMY_POSSIBILIES}/>
   </React.StrictMode>
 );
 
