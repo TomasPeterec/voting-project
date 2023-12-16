427937a3da65f3de42dd8cb2480b9a62bbb6c775
Tomas Peterec <info@tomaspeterec.sk>
2023-12-16 23:30:38 +0100

    to Votings list items added representations of buttons

--- src/dashboard/items/DVotingItem.js
+++ src/dashboard/items/DVotingItem.js
@@ -1,36 +1,86 @@
 import React from "react";
 import { Box } from "@mui/material";
-import mobileWidth from "../../cssAndMaterial/is-device";
 import { useMediaQuery } from "@mui/material";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { styles02 } from "../../cssAndMaterial/styles02";
+import mobileWidth from "../../cssAndMaterial/is-device";
 
-const DVotingItem = ({nameOfVotes}) => {
-  //breakpoint
-  const isMobile = useMediaQuery(`(max-width:${mobileWidth}px)`); 
-  // const writeSomething = (text) => console.log(text)
-  const MyBox = () => {
-    return(  
-      <Box style={{display: "flex"}}
-        borderRadius="10px"
-        border="1px solid #ccc"
-        p={2}
-      >
-        <div style={{width: "55px"}}>Staus</div>
-        {nameOfVotes}
-      </Box>
-    )
+const DVotingItem = ({ nameOfVotes }) => {
+  const [modalButtonsOn, setModalButtonsOn] = useState(false)
+  // Breakpoint
+  const isMobile = useMediaQuery(`(max-width:${mobileWidth}px)`);
+
+  const MyBox = ({ onClick }) => {
+    return (
+      <>
+        <div style={isMobile ? (
+          !modalButtonsOn ? (
+            styles02.desktopFormContainerHidden
+          ):(
+            styles02.displayed
+          )
+        ):(
+          styles02.desktopFormContainerHidden
+        )}>
+          <div style={{display: "flex", flexDirection: "column", height: "100%", justifyContent: "center"}}>
+            <h3 style={{display: "flex", justifyContent: "center", color: "white"}}>{nameOfVotes}</h3>
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <Button>Distr</Button>
+              <Button>Stats</Button>
+              <Button>Edit</Button>
+              <Button>Delete</Button>
+            </div>
+            <div style={{display: "flex", justifyContent: "center"}}>
+              <Button onClick={hideModalButtons}>Return</Button>
+            </div>
+          </div>
+        </div>
+        <Box
+          style={{ display: "flex", alignItems: "center", height: "30px" }}
+          borderRadius="10px"
+          border="1px solid #ccc"
+          p={"10px"}
+          onClick={onClick}
+        >
+          <div style={{width: "99%", display: "flex"}}>
+            <div style={{ width: "55px" }}>Status</div>
+            {nameOfVotes}
+          </div>
+          <div style={{width: "1%", display: "flex", justifyContent: "flex-end" }}>
+            {isMobile ? (
+              <></>
+            ) : (
+              <div style={{display: "flex"}}>
+                <Button>Distr</Button>
+                <Button>Stats</Button>
+                <Button>Edit</Button>
+                <Button>Delete</Button>
+              </div>
+            )}
+          </div>
+        </Box>
+      </>
+    );
+  };
+
+  const showModalButtons = () => {
+    setModalButtonsOn(true)
   }
 
-  return(  
+  const hideModalButtons = () => {
+    setModalButtonsOn(false)
+  }
+
+  return (
     <>
       {isMobile ? (
-          <MyBox />
-        ):(
-          <MyBox />
-        )
-
-      }
+        <MyBox onClick={showModalButtons} />
+      ) : (
+        <MyBox />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default DVotingItem
+export default DVotingItem;
